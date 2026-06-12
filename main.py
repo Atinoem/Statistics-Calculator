c6a7ea30b578906c68b7849c1996cea81aaae6fd
Atinoem <emilyatino14@gmail.com>
2026-06-12 09:25:19 +0300

    Median V3

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 print("User Input Calculator")
 data=input("Enter numbers seperated by spaces:")
 numbers=[float(x) for x in data.split()]
-mean=sum(numbers)/len(numbers)
-print("Numbers:",numbers)
-print("Mean:",mean)
+numbers.sort()
+print(numbers)
+mid=(len(numbers))//2
+if len(numbers)%2 ==1:
+    median=numbers[mid]
+else:
+    median=(numbers[(mid-1)]+numbers[mid])/2
+print("Median:",median)
+
